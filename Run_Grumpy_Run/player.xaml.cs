abac24617cc766d62e89e63a88dcd1449d1995d9
Florian <floflo.duch@gmail.com>
2015-04-08 19:22:59 +0200

    test

--- Run_Grumpy_Run/player.xaml.cs
+++ Run_Grumpy_Run/player.xaml.cs
@@ -47,7 +47,7 @@ namespace Run_Grumpy_Run
 
         public void MiseAJour(MainPage mp)
         {
-            if (DateTime.Now > horloge.AddMilliseconds(200))
+            if (DateTime.Now > horloge.AddMilliseconds(100))
             {
                 horloge = DateTime.Now;
                 int new_x = this.X;
